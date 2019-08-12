8cdc7d6a14ccbd8cb1891e2d90ff979256787422
Fernando Manechini <fmanechini@gmail.com>
2019-08-12 17:57:42 -0300

    retiradas linhas desnecessárias e comentários não pertinentes

--- exercicio_FernandoCalza_GabrielPerrucho/atvMonstrosArray.py
+++ exercicio_FernandoCalza_GabrielPerrucho/atvMonstrosArray.py
@@ -1,12 +1,5 @@
 from random import randint
 class Monstro:
-    nome = ""
-    tipo = ""
-    poderAtaque = 0
-    poderDefesa = 0
-    pontoVida = 0
-    grauEvo = 0
-    nivel = 0
     metodoAtaque = 0
     metodoDefesa = 0
     Defesas = []
@@ -26,58 +19,41 @@ class Monstro:
         self.grauEvo = grauEvo
         self.nivel = nivel
 
+    def MetodoAtq(self):
+        self.Ataques.clear()
+        for _ in range(10):
+            self.Ataques.append(self.poderAtaque * self.grauEvo * self.nivel * randint(1, 5))
+            
+        
+    
+    
+    def MetodoDef(self):
+        self.Defesas.clear()
+        for _ in range(10):
+            self.Defesas.append(self.poderDefesa * self.grauEvo * self.nivel * randint(1, 3))
 
-  #  def Dados(self, monstro): FORMA DE GABRIEL COM INPUT, DEIXAR PARA O FUTURO
-       # print("Digite o nome do Mob")
-       # nome = input
-       # print("Digite o  Tipo")
-        #tipo = input
-       # print("Digite o  Atk Power")
-       # poderAtaque = input
-       # print("Digite o  Def Power")
-       # poderDefesa = input
-        #print("Digite o  PV")
-       # pontoVida = input
-       # print("Digite o o Grau de Evo")
-       # grauEvo = input
-       # print("Digite o Nivel")
-       # nivel = input
-       # monstro = Monstro(nome,tipo,poderAtaque,poderDefesa,pontoVida,pontoVida,grauEvo,nivel)
-
-
-
-pikachu = Monstro("pikachu", "eletrico", 2, 2, 300, 1, 5)
-charmander = Monstro("charmander", "fogo", 3, 3, 250, 1, 7)
-acqua = Monstro("acqua", "agua", 3, 1, 260, 1, 6)
-fluff = Monstro("fluff", "terra", 1, 4, 270, 1, 4)
+pikachu = Monstro("pikachu", "eletrico", 3, 2, 300, 1, 5)
+charmander = Monstro("charmander", "fogo", 3, 3, 250, 1, 5)
+acqua = Monstro("acqua", "agua", 3, 1, 260, 1, 5)
+fluff = Monstro("fluff", "terra", 2, 3, 240, 1, 5)
 haleluia = Monstro("haleluia", "holy", 2, 2, 300, 1, 5)
-lilith = Monstro("lilith", "dark", 3, 3, 250, 1, 7)
-aerea = Monstro("aerea", "ar", 3, 1, 260, 1, 6)
-metal = Monstro("metal", "metal", 1, 4, 270, 1, 4)
+lilith = Monstro("lilith", "dark", 3, 3, 250, 1, 5)
+aerea = Monstro("aerea", "ar", 3, 2, 260, 1, 5)
+metal = Monstro("metal", "metal", 1, 2, 240, 1, 5)
 Competidores = [pikachu,charmander,acqua,fluff,haleluia,lilith,aerea,metal]
 
-def MetodoAtq(monstro):
-    monstro.Ataques.clear()
-    for _ in range(10):
-        monstro.Ataques.append(monstro.poderAtaque * monstro.grauEvo * monstro.nivel * randint(1, 5))
-        
-    
-
-
-def MetodoDef(monstro):
-    monstro.Defesas.clear()
-    for _ in range(10):
-        monstro.Defesas.append(monstro.poderDefesa * monstro.grauEvo * monstro.nivel * randint(1, 3))
-        
-    
-
 def batalha(monstro1,monstro2):
-    MetodoAtq(monstro1)
-    MetodoAtq(monstro2)
-    MetodoDef(monstro1)
-    MetodoDef(monstro2)
+    monstro1.MetodoAtq()
+    monstro2.MetodoAtq()
+    monstro1.MetodoDef()
+    monstro2.MetodoDef()
     Danos1 = []
     Danos2 = []
+    print(monstro1.Ataques)
+    print(monstro1.Defesas)
+    print(monstro2.Ataques)
+    print(monstro2.Defesas)
+    
     for numero in range(10):
         Danos1.append(monstro1.Ataques[numero] - monstro2.Defesas[numero])
         Danos2.append(monstro2.Ataques[numero] - monstro1.Defesas[numero])
